c999f77caab43e269c3bf27ebb9a79f52a3d779b
Michał Ordon <michal@tehidea.com>
2016-03-25 23:39:20 +0000

    Rename author(s)Page

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Home = require('./components/homePage');
-var Authors = require('./components/authors/authorsPage');
+var Authors = require('./components/authors/authorPage');
 var About = require('./components/about/aboutPage');
 var Header = require('./components/common/header');
 
