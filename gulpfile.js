0fb0650e6fadbe98b60a89471b4dbf8e62dc9f06
Michał Ordon <michal@tehidea.com>
2016-03-25 17:24:35 +0000

    Add browserify

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,19 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // Runs a local dev server
 var open = require('gulp-open'); // Opens an URI in a web browser
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
+
 
 var config = {
     port: 9005,
     devBaseUri: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 }
 
@@ -34,8 +40,19 @@ gulp.task('html', function() {
         .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-    gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind('console'))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
