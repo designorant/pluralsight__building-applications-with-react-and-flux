da9e7086aba110df3f0f98152339cc34ed2eb22b
Michał Ordon <michal@tehidea.com>
2016-03-27 23:23:14 +0100

    Add more routes

---
+++ src/components/notFoundPage.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+
+var NotFoundPage = React.createClass({
+
+    render: function() {
+        return (
+            <div>
+                <h1>Page Not Found</h1>
+                <p>Whoops! Sorry, there is nothing to see here.</p>
+                <p><Link to="app">Back to Home</Link></p>
+            </div>
+        );
+    }
+
+});
+
+module.exports = NotFoundPage;
--- src/routes.js
+++ src/routes.js
@@ -4,17 +4,22 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+var Redirect = ReactRouter.Redirect;
 
 var App = require('./components/app');
 var HomePage = require('./components/homePage');
 var AuthorPage = require('./components/authors/authorPage');
 var AboutPage = require('./components/about/aboutPage');
+var NotFoundPage = require('./components/notFoundPage');
 
 var routes = (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
         <Route path="authors" component={AuthorPage} />
         <Route path="about" component={AboutPage} />
+        <Redirect from="about-us" to="about"/>
+        <Redirect from="about/*" to="about"/>
+        <Route path="*" component={NotFoundPage}/>
     </Route>
 )
 
